bebc28d542c116fe22cb7e747917ac6aaa14caba
alexdavies440 <alexdavies440@gmail.com>
2024-10-14 21:56:02 -0500

    Stuck on form submission

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,7 +1,11 @@
 package org.launchcode.controllers;
 
+import org.launchcode.models.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +15,23 @@ public class UserController {
     @GetMapping("add")
     public String displayUserForm() {
         return "user/add";
+    }
+
+    @GetMapping("index")
+    public String displayIndex() {
+        return "user/index";
+    }
+    @PostMapping("add")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+//        model.addAttribute("username", user.getUsername());
+//        model.addAttribute("email", user.getEmail());
+//        model.addAttribute("password", user.getPassword());
+        verify = "password";
+
+        if (2 == 2) {
+            return "user/index";
+        } else {
+            return "user/add";
+        }
     }
 }
--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -3,7 +3,7 @@ package org.launchcode.models;
 public class User {
     private String username;
     private String email;
-    private String password;
+    private String password = "password";
 
     public User(String username, String email, String password) {
         this.username = username;
