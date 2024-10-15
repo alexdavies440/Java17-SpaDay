52cfa40cfe79617b2fbf24c7d7cb1314dbfcc76b
alexdavies440 <alexdavies440@gmail.com>
2024-10-14 20:31:10 -0500

    added form

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -1,10 +1,15 @@
 package org.launchcode.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
+    @GetMapping("add")
+    public String displayUserForm() {
+        return "user/add";
+    }
 }
