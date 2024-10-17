a423e3cdded1286eddc7a2f8a94895684f4fe8fb
alexdavies440 <alexdavies440@gmail.com>
2024-10-16 21:14:55 -0500

    Some updates after studio with Ariel

--- src/main/java/org/launchcode/controllers/UserController.java
+++ src/main/java/org/launchcode/controllers/UserController.java
@@ -17,37 +17,24 @@ public class UserController {
         return "user/add";
     }
 
-    @GetMapping("index")
-    public String displayIndex() {
-        return "user/index";
-    }
+//    @GetMapping("index")
+//    public String displayIndex() {
+//        return "user/index";
+//    }
 
-    @PostMapping("add")
+    @PostMapping("")
     public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        verify = "pie";
-        String error = "Please try again";
-        model.addAttribute("error", error);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-
         if (verify.equals(user.getPassword())) {
             return "user/index";
         } else {
+            model.addAttribute("error", "Passwords must match");
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
             return "user/add";
         }
     }
+//    @GetMapping("/detail")
+//
 
-//    @PostMapping("add")
-//    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-//        verify = "pie";
-//
-//
-//        if (user.getPassword().equals(verify)) {
-//            String message = "Welcome " + user.getUsername();
-//            model.addAttribute("message", message);
-//            return "user/index";
-//        } else {
-//            return "user/add";
-//        }
-//    }
 }
+
