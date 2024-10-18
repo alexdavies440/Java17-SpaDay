d54153e7e482e9d45ae8c473306659f33d69cd80
alexdavies440 <alexdavies440@gmail.com>
2024-10-17 21:19:15 -0500

    Will come back to finish this after some sleep

--- src/main/java/org/launchcode/models/User.java
+++ src/main/java/org/launchcode/models/User.java
@@ -1,9 +1,20 @@
 package org.launchcode.models;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class User {
+    @NotBlank(message = "username cannot be blank")
     private String username;
+
+    @Email(message = "Invalid email")
     private String email;
+
+    @NotBlank(message = "password cannot be blank")
     private String password;
+
+    @NotBlank(message = "Verify password field cannot be blank")
+    private String verify;
 
     public User() {
 
